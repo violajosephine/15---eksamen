bd71a02fae37c6b0c6c0792454f290ba501056a1
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-12-06 14:59:11 +0100

    wow okay going pretty good knock on wood

--- js/general.js
+++ js/general.js
@@ -1,18 +1,5 @@
-// document.querySelector(".om").addEventListener("click", nav);
-// document.querySelector(".terapi").addEventListener("click", nav);
-// document.querySelector(".priser").addEventListener("click", nav);
-// document.querySelector(".praktisk").addEventListener("click", nav);
-// document.querySelector(".blog").addEventListener("click", nav);
-// document.querySelector(".book").addEventListener("click", nav);
+document.querySelector("button").addEventListener("click", shadow);
 
-// function nav() {
-//   document.querySelector("#navbarNavAltMarkup").classList.remove("show");
-//   document.querySelector("button").setAttribute("aria-expanded", "false");
-//   document.querySelector("nav").classList.remove("nav-shadow");
-// }
-
-// document.querySelector("button").addEventListener("click", shadow);
-
-// function shadow() {
-//   document.querySelector("nav").classList.toggle("nav-shadow");
-// }
+function shadow() {
+  document.querySelector("nav").classList.toggle("nav-shadow");
+}
