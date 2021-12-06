e474dc41cf161432e3da58721ae77fe0ef23a338
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-12-06 14:12:01 +0100

    computer is boiling

---
+++ js/general.js
@@ -0,0 +1,18 @@
+// document.querySelector(".om").addEventListener("click", nav);
+// document.querySelector(".terapi").addEventListener("click", nav);
+// document.querySelector(".priser").addEventListener("click", nav);
+// document.querySelector(".praktisk").addEventListener("click", nav);
+// document.querySelector(".blog").addEventListener("click", nav);
+// document.querySelector(".book").addEventListener("click", nav);
+
+// function nav() {
+//   document.querySelector("#navbarNavAltMarkup").classList.remove("show");
+//   document.querySelector("button").setAttribute("aria-expanded", "false");
+//   document.querySelector("nav").classList.remove("nav-shadow");
+// }
+
+// document.querySelector("button").addEventListener("click", shadow);
+
+// function shadow() {
+//   document.querySelector("nav").classList.toggle("nav-shadow");
+// }
