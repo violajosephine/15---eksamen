777ba6a0606029fb153aad74c8fa46aa6ccd23d0
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-12-07 14:17:18 +0100

    I love math

--- js/general.js
+++ js/general.js
@@ -3,3 +3,11 @@ document.querySelector("button").addEventListener("click", shadow);
 function shadow() {
   document.querySelector("nav").classList.toggle("nav-shadow");
 }
+
+document.querySelector("#submit").addEventListener("click", submitted);
+function submitted() {
+  document.querySelector(".form_div").outerHTML =
+    "<h3>Tak for det. Du vil modtage en mail snarest!</h3>";
+
+  document.querySelector("#fredagsmail h3").style.padding = "50px 50px";
+}
