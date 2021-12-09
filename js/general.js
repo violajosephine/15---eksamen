fe38f50652f7086d471ba9c3aaee0714522c9b0b
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-12-09 23:13:21 +0100

    CV animation using greensock;)

--- js/general.js
+++ js/general.js
@@ -1,15 +1,5 @@
-document.querySelector("button").addEventListener("click", shadow);
+document.querySelector(".navbar-toggler").addEventListener("click", shadow);
 
 function shadow() {
   document.querySelector("nav").classList.toggle("nav-shadow");
-}
-
-document.querySelector("#submit").addEventListener("click", submitted);
-function submitted() {
-  document.querySelector(".form_div").outerHTML =
-    "<h3>Tak for det. Du vil modtage en mail snarest!</h3>";
-
-  document.querySelector("#fredagsmail h3").style.paddingTop = "50px";
-
-  document.querySelector("#meditation").classList.add("show");
 }
