f0516be4c7a78691a2ca774f44f2de12fa069f7b
Viola Josehine Hestbech <viol1397@stud.kea.dk>
2021-12-09 21:53:39 +0100

    damn I have the flu

--- js/general.js
+++ js/general.js
@@ -9,5 +9,7 @@ function submitted() {
   document.querySelector(".form_div").outerHTML =
     "<h3>Tak for det. Du vil modtage en mail snarest!</h3>";
 
-  document.querySelector("#fredagsmail h3").style.padding = "50px 50px";
+  document.querySelector("#fredagsmail h3").style.paddingTop = "50px";
+
+  document.querySelector("#meditation").classList.add("show");
 }
